366a329b97940ddf9c7aa7a1fa6f2cfae3c1335b
John <johnzhengyuliu@gmail.com>
2023-04-24 05:23:12 -0500

    use xorshift to calculate damage

--- programs/anchor-token/src/lib.rs
+++ programs/anchor-token/src/lib.rs
@@ -1,41 +1,25 @@
 use anchor_lang::prelude::*;
-use anchor_lang::solana_program::clock;
 use anchor_spl::{
     associated_token::AssociatedToken,
     metadata::{create_metadata_accounts_v3, CreateMetadataAccountsV3, Metadata},
     token::{burn, mint_to, Burn, Mint, MintTo, Token, TokenAccount},
 };
 use mpl_token_metadata::{pda::find_metadata_account, state::DataV2};
-use solana_program::{pubkey, pubkey::Pubkey};
-
-pub use switchboard_v2::{
-    OracleQueueAccountData, PermissionAccountData, SbState, VrfAccountData, VrfRequestRandomness,
-};
+use solana_program::{clock::Clock, pubkey, pubkey::Pubkey};
 
 declare_id!("4AGaHACpVPiht9vSFtEbtAQPcF5kMGLLKfcFjPhoUWgB");
 
 const ADMIN_PUBKEY: Pubkey = pubkey!("DfLZV18rD7wCQwjYvhTFwuvLh49WSbXFeJFPQb5czifH");
 const MAX_HEALTH: u8 = 100;
-const STATE_SEED: &[u8] = b"CLIENTSEED";
 
 #[error_code]
-#[derive(Eq, PartialEq)]
 pub enum ErrorCode {
     #[msg("Not enough health")]
     NotEnoughHealth,
-    #[msg("Switchboard VRF Account's authority should be set to the client's state pubkey")]
-    InvalidVrfAuthorityError,
-    #[msg("The max result must not exceed u64")]
-    MaxResultExceedsMaximum,
-    #[msg("Invalid VRF account provided.")]
-    InvalidVrfAccount,
-    #[msg("Not a valid Switchboard account")]
-    InvalidSwitchboardAccount,
 }
 
 #[program]
 pub mod anchor_token {
-
     use super::*;
 
     // Create new token mint with PDA as mint authority
@@ -90,139 +74,6 @@ pub mod anchor_token {
     // Create new player account
     pub fn init_player(ctx: Context<InitPlayer>) -> Result<()> {
         ctx.accounts.player_data.health = MAX_HEALTH;
-
-        let mut state = ctx.accounts.state.load_init()?;
-        *state = VrfClientState::default();
-        state.bump = ctx.bumps.get("state").unwrap().clone();
-        state.vrf = ctx.accounts.vrf.key();
-        state.max_result = MAX_HEALTH as u64;
-
-        emit!(VrfClientCreated {
-            vrf_client: ctx.accounts.state.key(),
-            max_result: MAX_HEALTH as u64,
-            timestamp: clock::Clock::get().unwrap().unix_timestamp
-        });
-
-        Ok(())
-    }
-
-    // This is basically "attack" in the game
-    pub fn request_randomness(
-        ctx: Context<RequestRandomness>,
-        permission_bump: u8,
-        switchboard_state_bump: u8,
-    ) -> Result<()> {
-        let client_state = ctx.accounts.state.load()?;
-        let bump = client_state.bump.clone();
-        let max_result = client_state.max_result;
-        drop(client_state);
-
-        let switchboard_program = ctx.accounts.switchboard_program.to_account_info();
-
-        let vrf_request_randomness = VrfRequestRandomness {
-            authority: ctx.accounts.state.to_account_info(),
-            vrf: ctx.accounts.vrf.to_account_info(),
-            oracle_queue: ctx.accounts.oracle_queue.to_account_info(),
-            queue_authority: ctx.accounts.queue_authority.to_account_info(),
-            data_buffer: ctx.accounts.data_buffer.to_account_info(),
-            permission: ctx.accounts.permission.to_account_info(),
-            escrow: ctx.accounts.escrow.clone(),
-            payer_wallet: ctx.accounts.payer_wallet.clone(),
-            payer_authority: ctx.accounts.payer_authority.to_account_info(),
-            recent_blockhashes: ctx.accounts.recent_blockhashes.to_account_info(),
-            program_state: ctx.accounts.program_state.to_account_info(),
-            token_program: ctx.accounts.token_program.to_account_info(),
-        };
-
-        let vrf_key = ctx.accounts.vrf.key();
-        let state_seeds: &[&[&[u8]]] = &[&[&STATE_SEED, vrf_key.as_ref(), &[bump]]];
-
-        msg!("requesting randomness");
-        vrf_request_randomness.invoke_signed(
-            switchboard_program,
-            switchboard_state_bump,
-            permission_bump,
-            state_seeds,
-        )?;
-
-        let mut client_state = ctx.accounts.state.load_mut()?;
-        client_state.result = 0;
-
-        emit!(RandomnessRequested {
-            vrf_client: ctx.accounts.state.key(),
-            max_result: max_result,
-            timestamp: clock::Clock::get().unwrap().unix_timestamp
-        });
-
-        msg!("randomness requested successfully");
-        Ok(())
-    }
-
-    // then "take damage" in the game
-    // this is the callback from the switchboard oracle
-    // request_randomness -> oracle -> consume_randomness
-    pub fn consume_randomness(ctx: Context<ConsumeRandomness>) -> Result<()> {
-        let vrf = ctx.accounts.vrf.load()?;
-        let result_buffer = vrf.get_result()?;
-        if result_buffer == [0u8; 32] {
-            msg!("vrf buffer empty");
-            return Ok(());
-        }
-
-        let state = &mut ctx.accounts.state.load_mut()?;
-        let max_result = state.max_result;
-        if result_buffer == state.result_buffer {
-            msg!("result_buffer unchanged");
-            return Ok(());
-        }
-
-        msg!("Result buffer is {:?}", result_buffer);
-        let value: &[u128] = bytemuck::cast_slice(&result_buffer[..]);
-        msg!("u128 buffer {:?}", value);
-        let result = value[0] % max_result as u128 + 1;
-        msg!("Current VRF Value [1 - {}] = {}!", max_result, result);
-
-        if state.result != result {
-            state.result_buffer = result_buffer;
-            state.result = result;
-            state.timestamp = clock::Clock::get().unwrap().unix_timestamp;
-
-            emit!(VrfClientUpdated {
-                vrf_client: ctx.accounts.state.key(),
-                max_result: state.max_result,
-                result: state.result,
-                result_buffer: result_buffer,
-                timestamp: state.timestamp,
-            });
-        }
-
-        // Subtract health from player based on vrf result
-        ctx.accounts.player_data.health =
-            ctx.accounts.player_data.health.saturating_sub(result as u8);
-
-        // PDA seeds and bump to "sign" for CPI
-        let seeds = b"reward";
-        let bump = *ctx.bumps.get("reward_token_mint").unwrap();
-        let signer: &[&[&[u8]]] = &[&[seeds, &[bump]]];
-
-        // CPI Context
-        let cpi_ctx = CpiContext::new_with_signer(
-            ctx.accounts.token_program.to_account_info(),
-            MintTo {
-                mint: ctx.accounts.reward_token_mint.to_account_info(),
-                to: ctx.accounts.player_token_account.to_account_info(),
-                authority: ctx.accounts.reward_token_mint.to_account_info(),
-            },
-            signer,
-        );
-
-        // Mint 1 token, accounting for decimals of mint
-        let amount = (1u64)
-            .checked_mul(10u64.pow(ctx.accounts.reward_token_mint.decimals as u32))
-            .unwrap();
-
-        mint_to(cpi_ctx, amount)?;
-
         Ok(())
     }
 
@@ -232,8 +83,18 @@ pub mod anchor_token {
         if ctx.accounts.player_data.health == 0 {
             return err!(ErrorCode::NotEnoughHealth);
         }
-        // Subtract 10 health from player
-        ctx.accounts.player_data.health = ctx.accounts.player_data.health.checked_sub(10).unwrap();
+        // Subtract health from player
+        let slot = Clock::get()?.slot;
+        let xorshift_output = xorshift64(slot);
+        let random_damage = xorshift_output % (MAX_HEALTH + 1) as u64;
+        msg!("Random damage: {}", random_damage);
+
+        ctx.accounts.player_data.health = ctx
+            .accounts
+            .player_data
+            .health
+            .checked_sub(random_damage as u8)
+            .unwrap();
 
         // PDA seeds and bump to "sign" for CPI
         let seeds = b"reward";
@@ -330,148 +191,6 @@ pub struct InitPlayer<'info> {
     #[account(mut)]
     pub player: Signer<'info>,
     pub system_program: Program<'info, System>,
-
-    // switchboard
-    // this is the account that will hold the VRF state (randomness)
-    #[account(
-        init,
-        seeds = [
-            STATE_SEED,
-            vrf.key().as_ref()
-        ],
-        payer = player,
-        space = 8 + std::mem::size_of::<VrfClientState>(),
-        bump,
-    )]
-    pub state: AccountLoader<'info, VrfClientState>,
-    #[account(
-        constraint = vrf.load()?.authority == state.key() @ ErrorCode::InvalidVrfAuthorityError
-    )]
-    pub vrf: AccountLoader<'info, VrfAccountData>,
-}
-
-#[derive(Accounts)]
-pub struct RequestRandomness<'info> {
-    #[account(
-        mut,
-        seeds = [
-            STATE_SEED,
-            vrf.key().as_ref(),
-        ],
-        bump = state.load()?.bump,
-        has_one = vrf @ ErrorCode::InvalidVrfAccount
-    )]
-    pub state: AccountLoader<'info, VrfClientState>,
-
-    // SWITCHBOARD ACCOUNTS
-    #[account(mut,
-        has_one = escrow
-    )]
-    pub vrf: AccountLoader<'info, VrfAccountData>,
-    #[account(mut,
-        has_one = data_buffer
-    )]
-    pub oracle_queue: AccountLoader<'info, OracleQueueAccountData>,
-    /// CHECK:
-    #[account(mut,
-        constraint =
-            oracle_queue.load()?.authority == queue_authority.key()
-    )]
-    pub queue_authority: UncheckedAccount<'info>,
-    /// CHECK
-    #[account(mut)]
-    pub data_buffer: AccountInfo<'info>,
-    #[account(mut)]
-    pub permission: AccountLoader<'info, PermissionAccountData>,
-    #[account(mut,
-        constraint =
-            escrow.owner == program_state.key()
-            && escrow.mint == program_state.load()?.token_mint
-    )]
-    pub escrow: Account<'info, TokenAccount>,
-    #[account(mut)]
-    pub program_state: AccountLoader<'info, SbState>,
-    /// CHECK:
-    #[account(
-        address = *vrf.to_account_info().owner,
-        constraint = switchboard_program.executable == true
-    )]
-    pub switchboard_program: AccountInfo<'info>,
-
-    // PAYER ACCOUNTS
-    #[account(mut,
-        constraint =
-            payer_wallet.owner == payer_authority.key()
-            && escrow.mint == program_state.load()?.token_mint
-    )]
-    pub payer_wallet: Account<'info, TokenAccount>,
-    /// CHECK:
-    #[account(mut, signer)]
-    pub payer_authority: AccountInfo<'info>,
-
-    // SYSTEM ACCOUNTS
-    /// CHECK:
-    #[account(address = solana_program::sysvar::recent_blockhashes::ID)]
-    pub recent_blockhashes: AccountInfo<'info>,
-    pub token_program: Program<'info, Token>,
-
-    // Initialize player token account if it doesn't exist
-    #[account(
-        init_if_needed,
-        payer = payer_authority,
-        associated_token::mint = reward_token_mint,
-        associated_token::authority = payer_authority
-    )]
-    pub player_token_account: Box<Account<'info, TokenAccount>>,
-    #[account(
-        mut,
-        seeds = [b"reward"],
-        bump,
-    )]
-    pub reward_token_mint: Box<Account<'info, Mint>>,
-    pub associated_token_program: Program<'info, AssociatedToken>,
-    pub system_program: Program<'info, System>,
-}
-
-#[derive(Accounts)]
-pub struct ConsumeRandomness<'info> {
-    #[account(
-        mut,
-        seeds = [
-            STATE_SEED,
-            vrf.key().as_ref(),
-        ],
-        bump = state.load()?.bump,
-        has_one = vrf @ ErrorCode::InvalidVrfAccount
-    )]
-    pub state: AccountLoader<'info, VrfClientState>,
-    pub vrf: AccountLoader<'info, VrfAccountData>,
-
-    // Test
-    #[account(
-        mut,
-        seeds = [b"player", player.key().as_ref()],
-        bump,
-    )]
-    pub player_data: Account<'info, PlayerData>,
-    /// CHECK:
-    pub player: AccountInfo<'info>,
-    // Initialize player token account if it doesn't exist
-    #[account(
-        mut,
-        associated_token::mint = reward_token_mint,
-        associated_token::authority = player
-    )]
-    pub player_token_account: Account<'info, TokenAccount>,
-
-    #[account(
-        mut,
-        seeds = [b"reward"],
-        bump,
-    )]
-    pub reward_token_mint: Account<'info, Mint>,
-    pub token_program: Program<'info, Token>,
-    pub associated_token_program: Program<'info, AssociatedToken>,
 }
 
 #[derive(Accounts)]
@@ -542,37 +261,10 @@ pub struct PlayerData {
     pub health: u8,
 }
 
-#[repr(packed)]
-#[account(zero_copy(unsafe))]
-#[derive(Default)]
-pub struct VrfClientState {
-    pub bump: u8,
-    pub max_result: u64,
-    pub result_buffer: [u8; 32],
-    pub result: u128,
-    pub timestamp: i64,
-    pub vrf: Pubkey,
-}
-
-#[event]
-pub struct VrfClientCreated {
-    pub vrf_client: Pubkey,
-    pub max_result: u64,
-    pub timestamp: i64,
-}
-
-#[event]
-pub struct RandomnessRequested {
-    pub vrf_client: Pubkey,
-    pub max_result: u64,
-    pub timestamp: i64,
-}
-
-#[event]
-pub struct VrfClientUpdated {
-    pub vrf_client: Pubkey,
-    pub max_result: u64,
-    pub result_buffer: [u8; 32],
-    pub result: u128,
-    pub timestamp: i64,
+pub fn xorshift64(seed: u64) -> u64 {
+    let mut x = seed;
+    x ^= x << 13;
+    x ^= x >> 7;
+    x ^= x << 17;
+    x
 }
--- tests/anchor-token.ts
+++ tests/anchor-token.ts
@@ -1,18 +1,14 @@
-import * as anchor from "@coral-xyz/anchor"
-import { AnchorProvider } from "@coral-xyz/anchor"
-import { Program } from "@coral-xyz/anchor"
+import * as anchor from "@project-serum/anchor"
+import { Program } from "@project-serum/anchor"
 import { AnchorToken } from "../target/types/anchor_token"
 import * as spl from "@solana/spl-token"
 import { assert } from "chai"
 import { Metaplex } from "@metaplex-foundation/js"
 import { PROGRAM_ID as TOKEN_METADATA_PROGRAM_ID } from "@metaplex-foundation/mpl-token-metadata"
 
-import * as sbv2 from "@switchboard-xyz/solana.js"
-import { NodeOracle } from "@switchboard-xyz/oracle"
-
 describe("anchor-token", () => {
-  const provider = AnchorProvider.env()
-  anchor.setProvider(provider)
+  // Configure the client to use the local cluster.
+  anchor.setProvider(anchor.AnchorProvider.env())
 
   const program = anchor.workspace.AnchorToken as Program<AnchorToken>
   const wallet = anchor.workspace.AnchorToken.provider.wallet
@@ -41,123 +37,6 @@ describe("anchor-token", () => {
     symbol: "GOLDSOL",
   }
 
-  // Keypair used to create new VRF account during setup
-  const vrfSecret = anchor.web3.Keypair.generate()
-  console.log(`VRF Account: ${vrfSecret.publicKey}`)
-
-  // PDA for VrfClientState Account, VRF Account is authority of this account
-  const [vrfClientKey] = anchor.web3.PublicKey.findProgramAddressSync(
-    [Buffer.from("CLIENTSEED"), vrfSecret.publicKey.toBytes()],
-    program.programId
-  )
-  console.log(`VRF Client: ${vrfClientKey}`)
-
-  const vrfIxCoder = new anchor.BorshInstructionCoder(program.idl)
-
-  // Callback to consume randomness (the instruction that the oracle CPI's back into our program)
-  const vrfClientCallback: sbv2.Callback = {
-    programId: program.programId,
-    accounts: [
-      // ensure all accounts in consumeRandomness are populated
-      { pubkey: vrfClientKey, isSigner: false, isWritable: true },
-      { pubkey: vrfSecret.publicKey, isSigner: false, isWritable: false },
-      { pubkey: playerPDA, isSigner: false, isWritable: true },
-      { pubkey: wallet.publicKey, isSigner: false, isWritable: false },
-      {
-        pubkey: playerTokenAccount,
-        isSigner: false,
-        isWritable: true,
-      },
-      {
-        pubkey: rewardTokenMintPda,
-        isSigner: false,
-        isWritable: true,
-      },
-      {
-        pubkey: spl.TOKEN_PROGRAM_ID,
-        isSigner: false,
-        isWritable: false,
-      },
-      {
-        pubkey: spl.ASSOCIATED_TOKEN_PROGRAM_ID,
-        isSigner: false,
-        isWritable: false,
-      },
-    ],
-    // ixData: Buffer.from(""),
-    ixData: vrfIxCoder.encode("consumeRandomness", ""), // pass any params for instruction here
-  }
-
-  let oracle: NodeOracle
-  let vrfAccount: sbv2.VrfAccount
-
-  // use this for localnet
-  let switchboard: sbv2.SwitchboardTestContext
-
-  // // use this for devnet
-  // let switchboard: {
-  //   program: sbv2.SwitchboardProgram
-  //   queue: sbv2.QueueAccount
-  // }
-
-  before(async () => {
-    // // use this for devnet
-    // const switchboardProgram = await sbv2.SwitchboardProgram.fromProvider(
-    //   provider
-    // )
-
-    // const [queueAccount, queue] = await sbv2.QueueAccount.load(
-    //   switchboardProgram,
-    //   "uPeRMdfPmrPqgRWSrjAnAkH78RqAhe5kXoW6vBYRqFX"
-    // )
-    // switchboard = { program: switchboardProgram, queue: queueAccount }
-
-    // use this for localnet
-    switchboard = await sbv2.SwitchboardTestContext.loadFromProvider(provider, {
-      // You can provide a keypair to so the PDA schemes dont change between test runs
-      name: "Test Queue",
-      keypair: sbv2.SwitchboardTestContextV2.loadKeypair(
-        "~/.keypairs/queue.json"
-      ),
-      queueSize: 10,
-      reward: 0,
-      minStake: 0,
-      oracleTimeout: 900,
-      unpermissionedFeeds: true,
-      unpermissionedVrf: true,
-      enableBufferRelayers: true,
-      oracle: {
-        name: "Test Oracle",
-        enable: true,
-        stakingWalletKeypair: sbv2.SwitchboardTestContextV2.loadKeypair(
-          "~/.keypairs/oracleWallet.json"
-        ),
-      },
-    })
-
-    oracle = await NodeOracle.fromReleaseChannel({
-      chain: "solana",
-      releaseChannel: "testnet",
-      network: "devnet", // disables production capabilities like monitoring and alerts
-      rpcUrl: switchboard.program.connection.rpcEndpoint,
-      oracleKey: switchboard.oracle.publicKey.toBase58(),
-      secretPath: switchboard.walletPath,
-      silent: true, // set to true to suppress oracle logs in the console
-      envVariables: {
-        VERBOSE: "1",
-        DEBUG: "1",
-        DISABLE_NONCE_QUEUE: "1",
-        DISABLE_METRICS: "1",
-      },
-    })
-
-    await oracle.startAndAwait()
-  })
-
-  after(async () => {
-    oracle?.stop()
-  })
-
   it("Initialize New Token Mint", async () => {
     const rewardTokenMintMetadataPDA = await metaplex
       .nfts()
@@ -180,23 +59,12 @@ describe("anchor-token", () => {
   })
 
   it("Init Player", async () => {
-    const queue = await switchboard.queue.loadData()
-
-    // Create Switchboard VRF and Permission account
-    ;[vrfAccount] = await switchboard.queue.createVrf({
-      callback: vrfClientCallback,
-      authority: vrfClientKey, // vrf authority
-      vrfKeypair: vrfSecret,
-      enable: !queue.unpermissionedVrfEnabled, // only set permissions if required
-    })
-
+    // Add your test here.
     const tx = await program.methods
       .initPlayer()
       .accounts({
         playerData: playerPDA,
         player: wallet.publicKey,
-        state: vrfClientKey,
-        vrf: vrfAccount.publicKey,
       })
       .rpc()
     console.log("Your transaction signature", tx)
@@ -205,112 +73,28 @@ describe("anchor-token", () => {
     assert(playerData.health === 100)
   })
 
-  it("request_randomness", async () => {
-    const queue = await switchboard.queue.loadData()
-    const vrf = await vrfAccount.loadData()
-
-    // derive the existing VRF permission account using the seeds
-    const [permissionAccount, permissionBump] = sbv2.PermissionAccount.fromSeed(
-      switchboard.program,
-      queue.authority,
-      switchboard.queue.publicKey,
-      vrfAccount.publicKey
-    )
-
-    // 0.002 wSOL fee for requesting randomness
-    const [payerTokenWallet] =
-      await switchboard.program.mint.getOrCreateWrappedUser(
-        switchboard.program.walletPubkey,
-        { fundUpTo: 0.002 }
-      )
-
-    // Request randomness
+  it("Kill Enemy to Mint 1 Token", async () => {
+    // Add your test here.
     const tx = await program.methods
-      .requestRandomness(permissionBump, switchboard.program.programState.bump)
+      .killEnemy()
       .accounts({
-        state: vrfClientKey,
-        vrf: vrfAccount.publicKey,
-        oracleQueue: switchboard.queue.publicKey,
-        queueAuthority: queue.authority,
-        dataBuffer: queue.dataBuffer,
-        permission: permissionAccount.publicKey,
-        escrow: vrf.escrow,
-        programState: switchboard.program.programState.publicKey,
-        switchboardProgram: switchboard.program.programId,
-        payerWallet: payerTokenWallet,
-        payerAuthority: wallet.publicKey,
-        recentBlockhashes: anchor.web3.SYSVAR_RECENT_BLOCKHASHES_PUBKEY,
-        tokenProgram: anchor.utils.token.TOKEN_PROGRAM_ID,
-
+        playerData: playerPDA,
         playerTokenAccount: playerTokenAccount,
         rewardTokenMint: rewardTokenMintPda,
       })
       .rpc()
-
     console.log("Your transaction signature", tx)
-
-    const result = await vrfAccount.nextResult(
-      new anchor.BN(vrf.counter.toNumber() + 1),
-      45_000
-    )
-    if (!result.success) {
-      throw new Error(`Failed to get VRF Result: ${result.status}`)
-    }
-
-    const vrfClientState = await program.account.vrfClientState.fetch(
-      vrfClientKey
-    )
-    console.log(`VrfClient Result: ${vrfClientState.result.toString(10)}`)
+    // assert.strictEqual(
+    //   Number(
+    //     (await connection.getTokenAccountBalance(playerTokenAccount)).value
+    //       .amount
+    //   ),
+    //   1_000_000_000
+    // )
 
     const playerData = await program.account.playerData.fetch(playerPDA)
-    console.log(`Player Health: ${playerData.health}`)
-
-    assert(playerData.health === 100 - vrfClientState.result.toNumber())
-    assert.strictEqual(
-      Number(
-        (await connection.getTokenAccountBalance(playerTokenAccount)).value
-          .amount
-      ),
-      1_000_000_000
-    )
-
-    const callbackTxnMeta = await vrfAccount.getCallbackTransactions()
-    console.log(
-      JSON.stringify(
-        callbackTxnMeta.map((tx) => tx.meta.logMessages),
-        undefined,
-        2
-      )
-    )
-
-    assert(
-      !vrfClientState.result.eq(new anchor.BN(0)),
-      "Vrf Client holds no result"
-    )
+    console.log("Player Health: ", playerData.health)
   })
-
-  // it("Kill Enemy to Mint 1 Token", async () => {
-  //   // Add your test here.
-  //   const tx = await program.methods
-  //     .killEnemy()
-  //     .accounts({
-  //       playerData: playerPDA,
-  //       playerTokenAccount: playerTokenAccount,
-  //       rewardTokenMint: rewardTokenMintPda,
-  //     })
-  //     .rpc()
-  //   console.log("Your transaction signature", tx)
-  //   assert.strictEqual(
-  //     Number(
-  //       (await connection.getTokenAccountBalance(playerTokenAccount)).value
-  //         .amount
-  //     ),
-  //     1_000_000_000
-  //   )
-
-  //   const playerData = await program.account.playerData.fetch(playerPDA)
-  //   assert(playerData.health === 90)
-  // })
 
   it("Burn 1 Token to Heal", async () => {
     // Add your test here.
